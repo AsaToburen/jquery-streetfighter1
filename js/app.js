eb6f9a424084ffb320de887ee2dcc38103958269
Asa Toburen <atopettit@gmail.com>
2014-11-24 13:25:50 -0700

    a few tweaks to the js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -12,17 +13,23 @@ $(document).ready(function() {
 	.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
 		.animate(
-	  		{'left': '300px'},
-	  		500,
+	  		{'left': '1000px'},
+	  		600,
 	  		function() {
 	  	  		$(this).hide();
-	 	  		$(this).css('left', '-212px');
+	 	  		$(this).css('left', '250px');
   	  		 }
 		);	
 	})
+	.mouseup(function () {
+		$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
+	});
+
 
 $(document).on('keydown', function(event) {
         if (event.which == 88) {
